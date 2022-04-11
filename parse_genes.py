547b699972d6f153f4ddbf1c9255d05de8f514f2
aileen-s <a.scott21@imperial.ac.uk>
2022-04-11 10:52:38 +0100

    parse_genes working, update gitignore

--- parse_genes.py
+++ parse_genes.py
@@ -1,17 +1,17 @@
-from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from Bio import SeqFeature
 
 from Bio import SeqIO
 COX1_Records = []
 for record in SeqIO.parse("test.gb", "gb"):
     for feature in record.features:
-        for k, v in feature.qualifiers.items():
-            if k=="gene" and v==["COX1"]:
-                if 
-                COX1_seq = record[feature.location.start:feature.location.end]
-                COX1_Records.append(COX1_seq)
+        for k, v in feature.qualifiers.items():     # Set up loop to search for key/value
+            if k=="gene" and v==["COX1"]:           # Key = "gene", Value = gene name
+                COX1_seq = record[feature.location.start:feature.location.end] # Extract sequence
+                COX1_Records.append(COX1_seq)       # Add sequences to list
 
+COX1full = []
 for record in COX1_Records:
-    print(feature.qualifiers.items)
+    if len(record.seq) >= 1300:     # From extracted COX1 records, refine to sequences over 1300bp
+        COX1fasta = record.format("fasta")
+        COX1full.append(COX1fasta)     # Add to new list
+        print(COX1full, file = open("COX1fullseqs.txt", "w"))
+
