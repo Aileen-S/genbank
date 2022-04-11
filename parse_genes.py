c4b872078d6630f265512f4e00d1a89433aa24f8
aileen-s <a.scott21@imperial.ac.uk>
2022-04-11 13:31:17 +0100

    Added name variants to parse_genes.

--- parse_genes.py
+++ parse_genes.py
@@ -1,17 +1,44 @@
 
 from Bio import SeqIO
+import urllib
+
+def loadnamevariants():
+    output = {}
+    url = "https://raw.githubusercontent.com/tjcreedy/genenames/master/gene_name_variants.txt"
+    for line in urllib.request.urlopen(url):
+        line = line.decode('utf-8').strip()
+        name = line.split(";")[0]
+        annotype = line.split(":")[0].split(";")[1]
+        variants = line.split(":")[1].split(",")
+        for v in variants:
+            for g in ['', ' ']:
+                v = v.replace(g, '')
+                for s in ['',' GENE', ' '+annotype.upper()]:
+                    output[v+s] = name
+    return(output)
+
+# Get the gene name variants
+genenames = loadnamevariants()
+
 COX1_Records = []
-for record in SeqIO.parse("test.gb", "gb"):
+for record in SeqIO.parse("COX1_longest.gb", "gb"):
     for feature in record.features:
-        for k, v in feature.qualifiers.items():     # Set up loop to search for key/value
-            if k=="gene" and v==["COX1"]:           # Key = "gene", Value = gene name
+        if feature.id in genenames:
+            stdname = genenames[feature.id]
+            if stdname==["COX1"]:
                 COX1_seq = record[feature.location.start:feature.location.end] # Extract sequence
                 COX1_Records.append(COX1_seq)       # Add sequences to list
 
+# Filter out longest sequences
 COX1full = []
 for record in COX1_Records:
-    if len(record.seq) >= 1300:     # From extracted COX1 records, refine to sequences over 1300bp
+    if len(record.seq) >= 600:             # From extracted COX1 records, refine to sequences over 1300bp
         COX1fasta = record.format("fasta")
-        COX1full.append(COX1fasta)     # Add to new list
+        COX1full.append(COX1fasta)          # Add to new list
         print(COX1full, file = open("COX1fullseqs.txt", "w"))
 
+# Check how many sequences
+GBfile = open("COX1fullseqs.txt")
+GBfile = GBfile.read()
+count = GBfile.count(">")
+print(str(count) + " records saved")
