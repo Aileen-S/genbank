13cdcbf316c646fd52387cf45117ffe4eabb4437
aileen-s <a.scott21@imperial.ac.uk>
2022-04-14 09:01:12 +0100

    Added loop counter

--- parse_genes.py
+++ parse_genes.py
@@ -1,10 +1,9 @@
-
 from Bio import SeqIO
 import urllib
 
 def loadnamevariants():
     output = {}
-    url = "https://raw.githubusercontent.com/tjcreedy/genenames/master/gene_name_variants.txt"
+    url = "https://raw.githubusercontent.com/tjcreedy/constants/master/gene_name_variants.txt"
     for line in urllib.request.urlopen(url):
         line = line.decode('utf-8').strip()
         name = line.split(";")[0]
@@ -30,15 +29,14 @@ for record in SeqIO.parse("COX1_longest.gb", "gb"):
                 COX1_Records.append(COX1_seq)       # Add sequences to list
 
 # Filter out longest sequences
-COX1full = []
-for record in COX1_Records:
-    if len(record.seq) >= 600:             # From extracted COX1 records, refine to sequences over 1300bp
-        COX1fasta = record.format("fasta")
+fh = open("COX1fullseqa.fasta", "w")        # Write new file
+n = 0                                       # Start count
+COX1full = []                               # Make empty list
+for record in COX1_Records:                 # Process records found from genbank search
+    if len(record.seq) >= 1000:             # Keep only sequences over set length
+        n += 1                              # Count kept sequences
+        COX1fasta = record.format("fasta")  # Format as fasta
         COX1full.append(COX1fasta)          # Add to new list
-        print(COX1full, file = open("COX1fullseqs.txt", "w"))
+        print(COX1full, file = open("COX1fullseqs.txt", "w"))   # Print to file
 
-# Check how many sequences
-GBfile = open("COX1fullseqs.txt")
-GBfile = GBfile.read()
-count = GBfile.count(">")
-print(str(count) + " records saved")
+print(str(n) + " records saved")
