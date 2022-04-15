13669c6a28c43004406205bb3c7ac64f5bcf6a31
aileen-s <a.scott21@imperial.ac.uk>
2022-04-15 17:28:21 +0100

    Initial commit

---
+++ genbank_to_CSV.py
@@ -0,0 +1,18 @@
+# Parse GenBank file to get CVS file with accession numbers and taxonomy
+
+import csv
+from Bio import SeqIO
+
+with open("COX1_longest.CSV", "w") as file:                                         # Open output file
+    csvfile = csv.writer(file)                                                      # Name writer object
+    csvfile.writerow(["Accessions", "Family", "Subfamily", "Tribe", "Genus",])      # Specify column names
+    for record in SeqIO.parse("COX1_longest.gb", "gb"):                             # Open GenBank file
+        Acc = record.id                                                             # Get accessions
+        Tax = record.annotations["taxonomy"]                                        # Get taxonomy list
+        Row = [Acc]                                     # New list with accession in first column
+        n = 0                                           # Start counter
+        for t in Tax:
+            n += 1                                      # +1 to counter for every loop
+            if n >= 13:                                 # Skip taxon levels we don't need
+                Row.append(t)                           # Add desired taxon levels to list
+        csvfile.writerow(Row)                           # Write accession/taxonomy list to row
