0588139882040f92aa31b081abeb61f7e4101cb6
aileen-s <a.scott21@imperial.ac.uk>
2022-04-20 12:45:29 +0100

    Add db_xref

--- genbank_to_CSV.py
+++ genbank_to_CSV.py
@@ -1,19 +1,25 @@
 # Parse GenBank file to get CSV file with accession numbers and taxonomy
+# "Domain", "Kingdom", "Superphylum", "Phylum", "Subphylum", "Class", "Subclass", "Infraclass", "Superorder", "Order", "Suborder", "Superfamily", "Family", "Subfamily", "Tribe"
+# Need to extract db_xref for csv file
 
 import csv
 from Bio import SeqIO
 
-with open("COX1_longest.CSV", "w") as file:             # Open output file
+with open("COX1_longest.csv", "w") as file:             # Open output file
     csvfile = csv.writer(file)                          # Name writer object
-    csvfile.writerow(["accession", "species", "family", "subfamily", "tribe"])  # Specify column names
+    csvfile.writerow(["Accession", "taxon_id", "Species", "Domain", "Kingdom", "Superphylum", "Phylum", "Subphylum", "Class", "Subclass", "Infraclass", "Superorder", "Order", "Suborder", "Superfamily", "Family", "Subfamily", "Tribe", "1", "2", "3"])  # Specify column names
     for record in SeqIO.parse("COX1_longest.gb", "gb"): # Open GenBank file
-        Acc = record.id                                 # Get accessions
-        Spe = record.annotations["organism"]
-        Tax = record.annotations["taxonomy"]            # Get taxonomy list
-        Row = [Acc, Spe]                                # New list with accession in first column
-        n = 0                                           # Start counter
+        Acc = record.id[0:-2]                                 # Get accessions
+        for feature in record.features:
+            for k, v in feature.qualifiers.items():
+                if k == "db_xref":                      # Search for db_xref (taxon ID)
+                    str_id = str(v)                     # Save as string
+                    if "taxon" in str_id:
+                        xref = str_id[8:-2]             # Extract numbers. Original format is eg ['taxon:207455']
+        Spe = record.annotations["organism"]            # Get genus/species
+        Tax = record.annotations["taxonomy"]            # Get higher taxonomy list
+        Row = [Acc, xref, Spe]                          # New list with accession and species names
         for t in Tax:
-            n += 1                                      # Count loops
-            if n >= 13 and n <= 15:                     # Skip taxon levels we don't need
-                Row.append(t)                           # Add desired taxon levels to list
+            Row.append(t)                               # Add desired taxon levels to list
         csvfile.writerow(Row)                           # Write accession/taxonomy list to row
+
