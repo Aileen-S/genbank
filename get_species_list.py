2e04edcf610b7e120805f8a5cdaed8d1bfeb13da
aileen-s <a.scott21@imperial.ac.uk>
2022-04-22 08:53:24 +0100

    Runs esearch for each species from input taxon

--- get_species_list.py
+++ get_species_list.py
@@ -7,31 +7,42 @@ import argparse
 from Bio import Entrez
 Entrez.email = "aileen.scott@nhm.ac.uk"
 
-
 parser = argparse.ArgumentParser(description="Get list of species names of requested taxa.")
 parser.add_argument("-t", "--taxon", type=str)  # Define command line inputs.
 #parser.add_argument("-g", "--gene", type=str)   # -- means argument is optional, input with flags
 #parser.add_argument("-l", "--length", type=str)
 args = parser.parse_args()         # Process input args from command line
 
-handle = Entrez.esearch(db="nucleotide", term=f"{args.taxon}", retmax=10)
+handle = Entrez.esearch(db="nucleotide", term=f"{args.taxon}", retmax=3)# Search for all records of specified taxon
 record = Entrez.read(handle)
-accs = record["IdList"]
-print(str(record["Count"]) + " records found")
+accs   = record["IdList"]                                               # Save accession numbers
+print(str(record["Count"]) + " records found")                          # Print total records
+print(record)
 
 accs_str = ",".join(accs)
-handle = Entrez.esummary(db="nucleotide", id=accs_str)
+handle = Entrez.esummary(db="nucleotide", id=accs_str)                  # Get esummary for accessions
 record = Entrez.read(handle)
-TaxList = []
-Accessions = []
-for rec in record:
-    #print(rec["TaxId"])
-    TaxId = rec["TaxId"]        # Look at db_xref taxon identifiers
-    if TaxId in TaxList:        # Check if taxon is already in the list
-        continue                # If so, skip
-    else:
-        TaxList.append(TaxId)           # If not, add to list
-        Accessions.append(rec["Id"])    # Save accession
-print(str(len(TaxList)) + " unique species saved")
+TaxSet = set()
+for rec in record:                                                      # Save taxon ids in a set (removes duplicates)
+    Tax = ("txid"+str(int(rec["TaxId"])))                               # Add "txid before number for esearch
+    TaxSet.add(Tax)
 
+print(str(len(TaxSet)) + " unique species saved")                       # Print total taxon ids
 
+Species = {}
+for Tax in TaxSet:
+    handle = Entrez.esearch(db="nucleotide", term=Tax)                  # Search for all records for each taxon id
+    record = Entrez.read(handle)
+    IdList = record["IdList"]                                           # Get accessions
+    IdStr  = ",".join(IdList)                                           # Join into string for efetch
+    handle = Entrez.efetch(db="nucleotide", id=IdStr, rettype="gb")     # Get GenBank record for accession list
+    record = handle.read()
+
+# Need to make dict with species, genes and sequences
+# Work out name variants
+
+#for k,v in Species.items():
+
+# Then efetch from GIs
+# Add mitochondrion to term
+# Max sequence length 20000[SLEN]
