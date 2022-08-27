3b7a611eb2304b605d57a64f6d80bdb3495faf2b
Aileen Scott <aileenscott@Aileens-Air.lan>
2022-08-27 13:04:39 +0100

    added CLI

---
+++ filter_fasta.py
@@ -0,0 +1,22 @@
+from Bio import SeqIO
+import argparse
+
+parser = argparse.ArgumentParser(description="Search GenBank, retrive gene sequences and save as fasta.")
+parser.add_argument("-i", "--input", type=str, help="Taxon of interest")
+parser.add_argument("-f", "--filter", type=str, help="Taxon of interest")
+parser.add_argument("-o", "--output", type=str, help="Taxon of interest")
+args = parser.parse_args()         # Process input args from command line
+
+input_file = "COX1.fasta"
+id_file = "test.txt"
+output_file = "ouput.fasta"
+
+with open(args.filter) as id_handle:
+    wanted = set(line.rstrip("\n").split(None, 1)[0] for line in id_handle)
+print(f"Found {len(wanted)} unique identifiers in {args.filter}.")
+print(f"Searching {args.input}.")
+records = (r for r in SeqIO.parse(args.input, "fasta") if r.id in wanted)
+count = SeqIO.write(records, args.output, "fasta")
+print(f"Saved {count} records from {args.input} to {args.output}")
+if count < len(wanted):
+    print("Warning %i IDs not found in %s" % (len(wanted) - count, input_file))
