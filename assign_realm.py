c80cb66f222004c05918459f11811706acdfeecd
Aileen Scott <aileenscott@Aileens-MacBook-Air.local>
2022-09-12 11:57:50 +0100

    new version

--- assign_realm.py
+++ assign_realm.py
@@ -1,41 +1,36 @@
-from csv import writer
-from csv import reader
 import csv
 
-file = open("biogeography.csv")
+Ambiguous = ['Mexico', 'Angola', 'Mozambique', 'Namibia', 'Algeria', 'Botswana', 'Chad', 'Democratic Republic of the Congo', 'Egypt', 'Liyba', 'Mauritania', 'Niger', 'Nigeria', 'Mali', 'Afghanistan', 'China', 'Saudi Arabia', 'Russia', 'Canada', 'USA', 'Mexico', 'Papua New Guinea', 'Argentina', 'Bolivia', 'Brazil', 'Chile', 'Peru']
+
+file = open("countries.txt")
 regions = {}
 
 lines = file.readlines()
 for line in lines:
     line = line.strip()
-    realm, country = line.split(":")
-    countries = country.split(",")
-    regions[realm] = countries
+    region, other = line.split(";")
+    subregion, countries = other.split(":")
+    countries = countries.split(",")
+    if region in regions:
+        regions[region][subregion] = countries
+    else:
+        regions[region] = {subregion: countries}
 
-#print(regions)
 
-with open("metadata220614.csv", "r") as input, open("test.csv", "w") as output:
-    reader = reader(input)
-    writer = writer(output)
-    next(reader, None)
-    writer.writerow(["Accession", "Realm", "Country"])
+x = 0
+with open("metadata_all.csv") as input, open("test.csv", "w") as output:
+    reader = csv.reader(input)
     for row in reader:
-        realm = ""
-        for key in regions.keys():
-            if row[21] in regions[key]:
-                realm = key
-        newrow = [row[0], realm, row[21]]
-        writer.writerow(newrow)
-
-# Make new csv instead with just accession and realm, then can copy and paste. Edit get_species list to include realm for next run.?
-
-
-# Append a column in existing csv using csv.reader / csv.writer classes
-def add_column_in_csv(input_file, output_file, transform_row):
-    with open(input_file, 'r') as read_obj, \
-            open(output_file, 'w', newline='') as write_obj:
-        csv_reader = reader(read_obj)                   # Create a csv.reader object from the input file object
-        csv_writer = writer(write_obj)                  # Create a csv.writer object from the output file object
-        for row in csv_reader:                          # Read each row of the input csv file as list
-            transform_row(row, csv_reader.line_num)     # Pass the list / row in the transform function to add column text for this row
-            csv_writer.writerow(row)                    # Write the updated row / list to the output file
+        x += 1
+        if x == 1:
+            writer = csv.writer(output)
+            writer.writerow(row)
+        else:
+            for region, sub in regions.items():
+                for subregion, countries in sub.items():
+                    if row[26] in countries:
+                        row[24] = region
+                        row[25] = subregion
+                if row[26] in Ambiguous:
+                    row[23] = 'area data needed'
+            writer.writerow(row)
