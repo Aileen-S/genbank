7867dd2ce51f938b8949e37efd7b58027cf93e18
Aileen Scott <aileenscott@Aileens-Air.lan>
2022-11-20 16:05:18 +0000

    Add gbid input file option

--- get_concat_recs.py
+++ get_concat_recs.py
@@ -1,7 +1,7 @@
 
 #python3 get_concat_recs.py -e mixedupvoyage@gmail.com -t Megadytes
 
-#python3 get_concat_recs.py -e mixedupvoyage@gmail.com -f COX1barcode_only.fasta.out -i both
+#python3 get_concat_recs.py -e mixedupvoyage@gmail.com -f test.txt -r gbid -i both
 
 # Make dict of subgenus names: new colum for genus, renamed from subgenus dict if necessary.
 
@@ -61,7 +61,8 @@ def search_nuc(term, summaries=False, chunk=10000):
 # Add option to find only mito genes, or only selected genes.
 parser = argparse.ArgumentParser(description="Search GenBank, retrieve gene sequences and save as fasta.")
 parser.add_argument("-t", "--taxon", type=str, help="Taxon of interest")
-parser.add_argument('-f', '--file', type=str, help="Input file with txid list")
+parser.add_argument('-f', '--file', type=str, help="Input file with accession or taxon ID list")
+parser.add_argument('-r', '--ref', choices=['txid', 'gbid'], help="If using --file option, specify accessions or taxon IDs.")
 parser.add_argument('-i', '--fasta_id', choices=['gbid', 'txid', 'both'], help="Choose identifiers for output fastas. Default is gbid.")
 parser.add_argument('-b', '--both', action="store_true", help="Print taxon ID and accession in output fastas.")
 parser.add_argument("-e", "--email", type=str, help="Your email registered with NCBI")
@@ -113,146 +114,161 @@ unrec_genes = set()
 unrec_species = []
 Entrez.email = args.email
 
-if args.file:
-    taxids = []
+if args.ref == 'gbid':
+    accs = []
     file = open(args.file)
     lines = file.readlines()
     for line in lines:
-        taxid = line.strip()
-        taxids.append(taxid)
+        acc = line.strip()
+        accs.append(acc)
 
-    # Generate search term to get all sequences in the search taxonomy
-    # - if -n option not used, then include "mitochondrial" in search term.
-if args.taxon:
-    basesearch = f"(\"{args.taxon}\"[Organism] OR \"{args.taxon}\"[All Fields])"
+else:
+    if args.ref  == 'txid':
+        taxids = []
+        file = open(args.file)
+        lines = file.readlines()
+        for line in lines:
+            taxid = line.strip()
+            taxids.append(taxid)
 
-    # Retrieve all taxids that represent tips of the NCBI Taxonomy tree
-    # Make the search generator
-    searchgen = search_nuc(term=basesearch, summaries=True, chunk=5000)
+        # Generate search term to get all sequences in the search taxonomy
+        # - if -n option not used, then include "mitochondrial" in search term.
+    if args.taxon:
+        basesearch = f"(\"{args.taxon}\"[Organism] OR \"{args.taxon}\"[All Fields])"
 
-    taxids = set()
-    i = 0
-    for gbids, summaries in searchgen:
-        # gbids, summaries = next(searchgen)
-        i += 1
-        taxa = set(int(s['TaxId']) for s in summaries)
-        taxids.update(taxa)
-        print(f"iteration={i}, returns={len(gbids)}, first gbid={gbids[0]}, first summary accession={summaries[0]['Caption']}, taxids in this iteration={len(taxa)}, total taxids={len(taxids)}")
+        # Retrieve all taxids that represent tips of the NCBI Taxonomy tree
+        # Make the search generator
+        searchgen = search_nuc(term=basesearch, summaries=True, chunk=5000)
 
-    # Some of these will be subspecies.
-    # You need to search them in NCBI Taxonomy to weed out the subspecies and generate a list of latin biomials.
-    # Then iterate through each of these binomials (not taxids as initially thought) to download the sequences etc
-    print(f"{len(taxids)} unique taxon IDs saved")
-    print("Searching GenBank")
-    print("Downloading GenBank records for taxon IDs 0 to 100" if len(taxids) > 100 else
-          f"Downloading GenBank records for taxon IDs 0 to {len(taxids)}")
+        taxids = set()
+        i = 0
+        for gbids, summaries in searchgen:
+            # gbids, summaries = next(searchgen)
+            i += 1
+            taxa = set(int(s['TaxId']) for s in summaries)
+            taxids.update(taxa)
+            print(f"iteration={i}, returns={len(gbids)}, first gbid={gbids[0]}, first summary accession={summaries[0]['Caption']}, taxids in this iteration={len(taxa)}, total taxids={len(taxids)}")
 
-    # Set accepted genes and minimum sequence lengths
-    #min = {"ATP6": 500, "ATP8": 100, "COX1": 500, "COX2": 500, "COX3": 500, "CYTB": 500, "ND1": 500, "ND2": 500, "ND3": 300, "ND4": 500, "ND4L": 200, "ND5": 500, "ND6": 400}
+        # Some of these will be subspecies.
+        # You need to search them in NCBI Taxonomy to weed out the subspecies and generate a list of latin biomials.
+        # Then iterate through each of these binomials (not taxids as initially thought) to download the sequences etc
+        print(f"{len(taxids)} unique taxon IDs saved")
+        print("Searching GenBank")
+        print("Downloading GenBank records for taxon IDs 0 to 100" if len(taxids) > 100 else
+              f"Downloading GenBank records for taxon IDs 0 to {len(taxids)}")
 
-x = 0  # Count taxids
-y = 0  # Count records saved
+        y = 0  # Count records saved
+        for tax in taxids:
+            y += 1
+            if y % 100 == 0:
+                print(f"Downloading GenBank records for taxon IDs {y+1} to {y+100}" if (y+100) < len(taxids) else
+                      f"Downloading GenBank records for taxon IDs {y+1} to {len(taxids)}")
+            handle = Entrez.esearch(db="nucleotide", term=f"txid{tax}")       # Search for all records for each taxon id
+            record = Entrez.read(handle)
+            accs   = record["IdList"]   # Get GBIDs
+
+# Search through GBIDs
 species = {}
 sequences = []
-for tax in taxids:
-    y += 1
-    if y % 100 == 0:
-        print(f"Downloading GenBank records for taxon IDs {y+1} to {y+100}" if (y+100) < len(taxids) else
-              f"Downloading GenBank records for taxon IDs {y+1} to {len(taxids)}")
-    handle = Entrez.esearch(db="nucleotide", term=f"txid{tax}")       # Search for all records for each taxon id
-    record = Entrez.read(handle)
-    accs   = record["IdList"]                                         # Get accessions
-    accstr = ",".join(accs)                                           # Join into string for efetch
-    handle = Entrez.efetch(db="nucleotide", id=accstr, rettype="gb", retmode="text")  # Get GenBanks
-    record = SeqIO.parse(handle, "gb")
-    for rec in record:
+x = 0  # Count taxids
+accstr = ",".join(accs)                                           # Join into string for efetch
+print(accstr)
+handle = Entrez.efetch(db="nucleotide", id=accstr, rettype="gb", retmode="text")  # Get GenBanks
+record = SeqIO.parse(handle, "gb")
+for rec in record:
+    print(rec)
+    if args.taxon:
         if args.taxon not in rec.annotations["taxonomy"]:
             unrec_species.append(rec.name)
             continue
-        spec = rec.annotations["organism"]
-        specfasta = spec.replace(" ", "_")
-        taxonomy = rec.annotations["taxonomy"][10:15]
-        taxonomy.extend([""] * (5 - len(taxonomy)))
-        if taxonomy[4] == "Cybistrini":
-            taxonomy[3] = "Cybistrinae"
-        fastatax = f"{taxonomy[2]}_{taxonomy[3]}_{taxonomy[4]}_{specfasta}"
+    db_xref = rec.features[0].qualifiers["db_xref"]
+    for ref in db_xref:
+        if "taxon" in ref:  # Get NCBI taxon, rather than BOLD cross ref
+            txid = "".join(filter(str.isdigit, ref))  # Extract numbers from NCBI taxon value
+    spec = rec.annotations["organism"]
+    specfasta = spec.replace(" ", "_")
+    taxonomy = rec.annotations["taxonomy"][10:15]
+    taxonomy.extend([""] * (5 - len(taxonomy)))
+    if taxonomy[4] == "Cybistrini":
+        taxonomy[3] = "Cybistrinae"
+    fastatax = f"{taxonomy[2]}_{taxonomy[3]}_{taxonomy[4]}_{specfasta}"
 
-        if "country" in rec.features[0].qualifiers:
-            location = rec.features[0].qualifiers["country"][0]
-            if ":" in location:
-                country, region = location.split(":")
-            else:
-                country = location
-                region = ""
+    if "country" in rec.features[0].qualifiers:
+        location = rec.features[0].qualifiers["country"][0]
+        if ":" in location:
+            country, region = location.split(":")
         else:
-            country = ""
+            country = location
             region = ""
-        if "lat_lon" in rec.features[0].qualifiers:
-            latlon = rec.features[0].qualifiers["lat_lon"][0]
-        else:
-            latlon = ""
-        if "collection_date" in rec.features[0].qualifiers:
-            c_date = rec.features[0].qualifiers["collection_date"][0]
-        else:
-            c_date = ""
-        refs = []
-        for ref in rec.annotations["references"]:
-            refs.append(ref.authors)
-            refs.append(ref.title)
-            refs.append(ref.journal)
+    else:
+        country = ""
+        region = ""
+    if "lat_lon" in rec.features[0].qualifiers:
+        latlon = rec.features[0].qualifiers["lat_lon"][0]
+    else:
+        latlon = ""
+    if "collection_date" in rec.features[0].qualifiers:
+        c_date = rec.features[0].qualifiers["collection_date"][0]
+    else:
+        c_date = ""
+    refs = []
+    for ref in rec.annotations["references"]:
+        refs.append(ref.authors)
+        refs.append(ref.title)
+        refs.append(ref.journal)
 
-        for feature in rec.features:
-            type = feature.type
-            if type not in ('CDS', 'rRNA'):
-                continue  # skip the rest of the current iteration of this loop
-            name = get_feat_name(feature)                       # Find gene name
-            stdname = ""
-            for k, v in genes.items():
-                if name in v:
-                    stdname = k
-            if stdname == '':
-                continue
-            if stdname == "":
-                unrec_genes.add(name)
-                continue
-            if stdname in cds:
-                if 'codon_start' in feature.qualifiers:
-                    frame = feature.qualifiers["codon_start"]
-                else:
-                    frame = ''
-                    print(f"Reading frame missing from record {rec.name}, {stdname}.")
+    for feature in rec.features:
+        type = feature.type
+        if type not in ('CDS', 'rRNA'):
+            continue  # skip the rest of the current iteration of this loop
+        name = get_feat_name(feature)                       # Find gene name
+        stdname = ""
+        for k, v in genes.items():
+            if name in v:
+                stdname = k
+        if stdname == '':
+            continue
+        if stdname == "":
+            unrec_genes.add(name)
+            continue
+        if stdname in cds:
+            if 'codon_start' in feature.qualifiers:
+                frame = feature.qualifiers["codon_start"]
             else:
                 frame = ''
-            seq = feature.extract(rec.seq)
-            sequences.append(seq)
-            output = {"gene": stdname,
-                      "gbid": rec.name,
-                      "txid": tax,
-                      "description": rec.description,
-                      "spec": rec.annotations["organism"],
-                      "rec date": rec.annotations["date"],
-                      "c date": c_date,
-                      "taxonomy": taxonomy,
-                      "fastatax": fastatax,
-                      "type": type,
-                      "length": len(seq),
-                      "seq": seq,
-                      "frame": frame,
-                      "country": country,
-                      "region": region,
-                      "latlon": latlon,
-                      "refs": refs}
-            if tax in species:                              # If taxon ID in dict
-                if stdname in species[tax]:                 # If gene in dict for that taxon ID
-                    species[tax][stdname].append(output)    # Add gene info list to dict
-                    x += 1
-                else:
-                    species[tax][stdname] = [output]      # Otherwise add to dict with new key
-                    x += 1
-            else:
-                species[tax] = {stdname: [output]}      # Otherwise add to dict with new key
+                print(f"Reading frame missing from record {rec.name}, {stdname}.")
+        else:
+            frame = ''
+        seq = feature.extract(rec.seq)
+        sequences.append(seq)
+        output = {"gene": stdname,
+                  "gbid": rec.name,
+                  "txid": txid,
+                  "description": rec.description,
+                  "spec": rec.annotations["organism"],
+                  "rec date": rec.annotations["date"],
+                  "c date": c_date,
+                  "taxonomy": taxonomy,
+                  "fastatax": fastatax,
+                  "type": type,
+                  "length": len(seq),
+                  "seq": seq,
+                  "frame": frame,
+                  "country": country,
+                  "region": region,
+                  "latlon": latlon,
+                  "refs": refs}
+        if txid in species:                              # If taxon ID in dict
+            if stdname in species[txid]:                 # If gene in dict for that taxon ID
+                species[txid][stdname].append(output)    # Add gene info list to dict
                 x += 1
-            #break
+            else:
+                species[txid][stdname] = [output]      # Otherwise add to dict with new key
+                x += 1
+        else:
+            species[txid] = {stdname: [output]}      # Otherwise add to dict with new key
+            x += 1
+        #break
 
 
 print(f"\n{x} gene records saved to species dict")
