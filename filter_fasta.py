988b59442203901b02266c84ce5b9f7491078050
Aileen Scott <aileenscott@Aileens-Air.lan>
2023-01-06 17:49:51 +0000

    update

--- filter_fasta.py
+++ filter_fasta.py
@@ -1,20 +1,24 @@
 from Bio import SeqIO
 import argparse
 
-
 parser = argparse.ArgumentParser(description="Filter fasta file from list of IDs.")
-parser.add_argument("-i", "--input", type=str, help="Taxon of interest")
-parser.add_argument("-f", "--filter", type=str, help="Taxon of interest")
-parser.add_argument("-o", "--output", type=str, help="Taxon of interest")
+parser.add_argument("-i", "--input", type=str, help="File to be filtered")
+parser.add_argument("-f", "--filter", type=str, help="ID list")
+parser.add_argument("-o", "--output", type=str, help="Output file")
 args = parser.parse_args()
-
 
 with open(args.filter) as id_handle:
     wanted = set(line.rstrip("\n").split(None, 1)[0] for line in id_handle)
+
 print(f"Found {len(wanted)} unique identifiers in {args.filter}.")
 print(f"Searching {args.input}.")
+
 records = (r for r in SeqIO.parse(args.input, "fasta") if r.id in wanted)
 count = SeqIO.write(records, args.output, "fasta")
-print(f"Saved {count} records from {args.input} to {args.output}")
+
 if count < len(wanted):
-    print("Warning %i IDs not found in %s" % (len(wanted) - count, args.input))
+    print(f"{len(wanted) - count} IDs from {args.filter} not found in {args.input}")
+print(f"Saved {count} records from {args.input} to {args.output}")
+
+
+
