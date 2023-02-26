33c5cec558b9e44f591942920fe6e52b283c4112
A-Scott <a.scott21@imperial.ac.uk>
2023-02-26 23:39:28 +0000

    add hmm output option

--- filter_fasta.py
+++ filter_fasta.py
@@ -1,21 +1,24 @@
 from Bio import SeqIO
 import argparse
 
-parser = argparse.ArgumentParser(description="Filter fasta file from list of IDs. Choose either -p, -s or -f flag.")
+parser = argparse.ArgumentParser(description="Filter fasta file from list of IDs. Choose either -p, -s, -h or -f flag.")
 parser.add_argument("-i", "--input", type=str, help="Input fasta to be filtered")
-parser.add_argument('-p', '--partial_file', type=str, help='File containing terms/IDs to search for in sequence identifiers')
-parser.add_argument('-s', '--string', type=str, help='Single string to search for in sequence identifiers')
-parser.add_argument("-f", "--filter", type=str, help="File containing list of sequence identifiers to extract from input file")
 parser.add_argument("-o", "--output", type=str, help="Output file")
+
+# Filter options
+parser.add_argument('-s', '--string', type=str, help='Single string to search for in sequence identifiers')
+parser.add_argument('-p', '--partial_file', type=str, help='File containing terms/IDs to search for in sequence identifiers')
+parser.add_argument("-f", "--filter", type=str, help="File containing list of sequence identifiers to extract from input file")
+parser.add_argument("-hm", "--hmmer", type=str, help="File containing HMM output from --tblout flag")
+
 args = parser.parse_args()
 
 #args = argparse.Namespace(input='ATP6.fasta', filter='test.txt')
 
 if args.partial_file:
-    with open(args.partial_file) as id_handle:
-        ids = set(line.rstrip("\n").split(None, 1)[0] for line in id_handle)
+    with open(args.partial_file) as infile:
+        ids = set(line.rstrip("\n").split(None, 1)[0] for line in infile)
         print(f"Found {len(ids)} unique identifiers in {args.filter}")
-        print(f"Searching {args.input}")
         wanted = []
         for r in SeqIO.parse(args.input, "fasta"):
             for i in ids:
@@ -30,12 +33,23 @@ elif args.string:
             if i in r.id:
                 wanted.append(r.id)
 
-else:
-    with open(args.filter) as id_handle:
-        wanted = set(line.rstrip("\n").split(None, 1)[0] for line in id_handle)
+elif args.hmmer:
+    wanted = []
+    with open(args.hmmer) as infile:
+        lines = infile.readlines()
+        for line in lines:
+            if '#' in line:
+                continue
+            line = line.split(' ')
+            wanted.append(line[0])
 
-    print(f"Found {len(wanted)} unique identifiers in {args.filter}")
-    print(f"Searching {args.input}")
+
+else:
+    with open(args.filter) as infile:
+        wanted = set(line.rstrip("\n").split(None, 1)[0] for line in infile)
+
+print(f"Found {len(wanted)} unique identifiers in {args.filter}")
+print(f"Searching {args.input}")
 
 records = (r for r in SeqIO.parse(args.input, "fasta") if r.id in wanted)
 count = SeqIO.write(records, args.output, "fasta")
