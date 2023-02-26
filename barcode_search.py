44b3188739472a429f6c508fbb9b62d9a36352e4
A-Scott <a.scott21@imperial.ac.uk>
2023-02-26 22:56:14 +0000

    add fasta return option

--- barcode_search.py
+++ barcode_search.py
@@ -5,9 +5,10 @@ import argparse
 
 # Argument parser
 parser = argparse.ArgumentParser(description="Download GenBank files for requested taxon.")
-parser.add_argument("-t", "--taxon", type=str, help="Taxon of interest")
+parser.add_argument("-t", "--taxon", type=str, required=True, help="Taxon of interest")
 parser.add_argument("-e", "--email", type=str, help="Your email registered with NCBI")
-parser.add_argument("-o", "--output", type=str, help="Output file name")
+parser.add_argument("-o", "--output", type=str, required=True, help="Output file name")
+parser.add_argument('-f', '--format', choices=['gb', 'fasta'], required=True, help='Choose output format genbank or fasta')
 
 args = parser.parse_args()         # Process input args from command line
 
@@ -36,13 +37,13 @@ for start in range(0, count, 10000):
     gbids.append(','.join(record['IdList']))
 
 # Use GBIDs to download GenBank records
-print('Downloading GenBank records')
 z = 0
 for gbid_str in gbids:
     print(f"Downloading GenBank records for taxon IDs {z + 1} to {z + 1000}" if (z + 1000) < count else
           f"Downloading GenBank records for taxon IDs {z + 1} to {count}")
     z += 1000
-    handle = Entrez.efetch(db="nucleotide", id=gbid_str, rettype="gb", retmode="text")
+    handle = Entrez.efetch(db="nucleotide", id=gbid_str, rettype=args.format, retmode="text")
     output.write(handle.read())
-print(f'Output saved to {args.output}')
+print(f'Output saved in fasta format to {args.output}' if args.format == 'fasta' else
+      f'Output saved in GenBank flat file format to {args.output}')
 
