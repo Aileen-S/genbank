54a718b9344b988e6ab20bc1b4965649085d9fee
A-Scott <a.scott21@imperial.ac.uk>
2023-03-02 23:06:31 +0000

    update

--- write_constraint.py
+++ write_constraint.py
@@ -4,20 +4,20 @@ import argparse
 parser = argparse.ArgumentParser(description="Filter fasta file from list of IDs. Choose either -p, -s, -h or -f flag.")
 parser.add_argument("-i", "--ingroup", type=str, help="Ingroup fasta")
 parser.add_argument("-o", "--outgroup", type=str, help="Outgroup fasta")
+args = parser.parse_args()
 
 fast = open("fasttree_constraint.txt", 'w')
+rax = open('raxml_constraint.txt', 'w')
 ins = []
-args = parser.parse_args()
+
 for taxon in SeqIO.parse(args.ingroup, "fasta"):
     ins.append(taxon.id)
-fast.write(f'{len(ins) + 2} 2\n')
-for id in ins:
-    fast.write(f'{id} 1\n')
+    fast.write(f'>{taxon.id}\n1\n')
 
 outs = []
 for taxon in SeqIO.parse(args.outgroup, "fasta"):
     outs.append(taxon.id)
-    fast.write(f'{taxon.id} 0\n')
+    fast.write(f'>{taxon.id}\n0\n')
 
 with open('raxml_constraint.txt', 'w') as rax:
     rax.write(f'(({",".join(outs)}),({",".join(ins)}));')
