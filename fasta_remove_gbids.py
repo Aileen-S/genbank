134aa9fb24b9991a00b676496cd391c30c2f55d1
A-Scott <a.scott21@imperial.ac.uk>
2023-03-24 22:30:02 +0000

    update

--- fasta_remove_gbids.py
+++ fasta_remove_gbids.py
@@ -1,6 +1,6 @@
 import argparse
 
-parser = argparse.ArgumentParser(description="Get list of accessions or taxon IDs from fasta or list of sequence names.")
+parser = argparse.ArgumentParser(description="Remove accessions from fasta.")
 parser.add_argument("-i", "--input", type=str, help="in file")
 parser.add_argument("-o", "--output", type=str, help="out file")
 
@@ -11,14 +11,17 @@ file = open(args.input)
 output = open(args.output, 'w')
 lines = file.readlines()
 for line in lines:
-    if line.startswith('>'):
-        name = line.split("_")
-        if "NC" in name:
-            del name[1:3]
-        else:
-            if any(n.isdigit() for n in name[1]):
-                del name[1]
-        line = '_'.join(name)
+    try:
+        if line.startswith('>'):
+            name = line.split("_")
+            if "NC" in name:
+                del name[1:3]
+            else:
+                if any(n.isdigit() for n in name[1]):
+                    del name[1]
+            line = '_'.join(name)
+    except IndexError:
+        pass
     output.write(line)
 
 
