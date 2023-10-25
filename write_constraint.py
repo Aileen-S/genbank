15245b1a1f1cc00a0d4aafafe2ebb67e743df621
Aileen-S <a.scott21@imperial.ac.uk>
2023-10-25 10:50:23 +0100

    remove gbids

--- write_constraint.py
+++ write_constraint.py
@@ -4,11 +4,46 @@ import argparse
 parser = argparse.ArgumentParser(description="Filter fasta file from list of IDs. Choose either -p, -s, -h or -f flag.")
 parser.add_argument("-i", "--ingroup", type=str, help="Ingroup fasta")
 parser.add_argument("-o", "--outgroup", type=str, help="Outgroup fasta")
+parser.add_argument('-g', '--gbids', action='store_true', help='Remove GBIDs from input file')
 args = parser.parse_args()
 
 fast = open("fasttree_constraint.txt", 'w')
 rax = open('raxml_constraint.txt', 'w')
 ins = []
+
+if args.gbids:
+    file = open(args.ingroup)
+    output = open(f'{args.ingroup}.out', 'w')
+    lines = file.readlines()
+    for line in lines:
+        try:
+            if line.startswith('>'):
+                name = line.split("_")
+                if "NC" in name:
+                    del name[1:3]
+                else:
+                    if any(n.isdigit() for n in name[1]):
+                        del name[1]
+                line = '_'.join(name)
+        except IndexError:
+            pass
+        output.write(line)
+    file = open(args.outgroup)
+    output = open(f'{args.outgroup}.out', 'w')
+    lines = file.readlines()
+    for line in lines:
+        try:
+            if line.startswith('>'):
+                name = line.split("_")
+                if "NC" in name:
+                    del name[1:3]
+                else:
+                    if any(n.isdigit() for n in name[1]):
+                        del name[1]
+                line = '_'.join(name)
+        except IndexError:
+            pass
+        output.write(line)
 
 for taxon in SeqIO.parse(args.ingroup, "fasta"):
     ins.append(taxon.id)
