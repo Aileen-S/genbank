8154d890de5e93902d687ddfb5b53a59dfea7512
Aileen-S <a.scott21@imperial.ac.uk>
2023-11-06 11:48:11 +0000

    multiple input files

--- write_constraint.py
+++ write_constraint.py
@@ -1,24 +1,33 @@
 from Bio import SeqIO
 import argparse
 
-parser = argparse.ArgumentParser(description="Filter fasta file from list of IDs. Choose either -p, -s, -h or -f flag.")
+parser = argparse.ArgumentParser(description="Filter fasta file from list of IDs. Separate multiple files with commas.")
 parser.add_argument("-i", "--ingroup", type=str, help="Ingroup fasta")
 parser.add_argument("-o", "--outgroup", type=str, help="Outgroup fasta")
 args = parser.parse_args()
 
-fast = open("fasttree_constraint.txt", 'w')
-rax = open('raxml_constraint.txt', 'w')
 ins = []
-
-for taxon in SeqIO.parse(args.ingroup, "fasta"):
-    ins.append(taxon.id)
-    fast.write(f'>{taxon.id}\n1\n')
+ingroup = args.ingroup.split(',')
+for file in ingroup:
+    for taxon in SeqIO.parse(file, "fasta"):
+        ins.append(taxon.id)
+        #fast.write(f'>{taxon.id}\n1\n')
 
 outs = []
-for taxon in SeqIO.parse(args.outgroup, "fasta"):
-    outs.append(taxon.id)
-    fast.write(f'>{taxon.id}\n0\n')
+outgroup = args.output.split(',')
+for file in outgroup:
+    for taxon in SeqIO.parse(file, "fasta"):
+        outs.append(taxon.id)
+        #fast.write(f'>{taxon.id}\n0\n')
+
+with open('fasttree_constraint.txt', 'w') as fast:
+    for i in ins:
+        fast.write(f'{i}\n1\n')
+    for o in outs:
+        fast.write(f'{o}\n0\n')
 
 with open('raxml_constraint.txt', 'w') as rax:
     rax.write(f'(({",".join(outs)}),({",".join(ins)}));')
+
+
 
