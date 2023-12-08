67dad33201bfc4a421e4a45b9f2e7e39ea06aa90
Aileen-S <a.scott21@imperial.ac.uk>
2023-12-08 16:46:11 +0000

    add not found option for partial list

--- filter_fasta.py
+++ filter_fasta.py
@@ -15,17 +15,17 @@ parser.add_argument('-t', '--type', type=str, choices=['list', 'fasta', 'hmmer']
 args = parser.parse_args()         # Process input args from command line
 #args = argparse.Namespace(input='ATP6.fasta', filter='test.txt')
 
-wanted = []
+filter = []
 with open(args.search) as infile:
     if args.type == 'list':
         lines = infile.readlines()
         for line in lines:
-            wanted.append(line.strip())
+            filter.append(line.strip())
 
     elif args.type == 'fasta':
         recs = SeqIO.parse(args.search, "fasta")
         for rec in recs:
-            wanted.append(rec.id)
+            filter.append(rec.id)
 
     elif args.type == 'hmmer':
         lines = infile.readlines()
@@ -33,38 +33,31 @@ with open(args.search) as infile:
             if '#' in line:
                 continue
             line = line.split(' ')
-            wanted.append(line[0])
-print(f"{len(wanted)} sequence IDs in {args.search}")
+            filter.append(line[0])
 
 records = SeqIO.parse(args.input, "fasta")
 print(f'{len(list(records))} records in {args.input}')
 
 if args.partial:
-    if args.found:
-        found = open(args.found, 'w')
-        found = open(args.found, 'a')
-        records = SeqIO.parse(args.input, "fasta")
-        x = 0
-        for rec in records:
-            for w in wanted:
-                if w in rec.id:
-                    found.write(f'>{rec.id}\n{rec.seq}\n')
-                    x += 1
-        print(f"{x} of {len(wanted)} records from {args.search} found in {args.input}")
-        print(f"{x} records from {args.input} saved to {args.found}")
-    # Need to add notfound output option
+    filter_ids = []
+    records = SeqIO.parse(args.input, "fasta")
+    x = 0
+    for rec in records:
+        for w in filter:
+            if w in rec.id:
+                filter_ids.append(rec.id)
+    filter = filter_ids
 
-else:
-    if args.found:
-        records = (r for r in SeqIO.parse(args.input, "fasta") if r.id in wanted)
-        count = SeqIO.write(records, args.found, "fasta")
-        print(f"{count} of {len(wanted)} records from {args.search} found in {args.input}")
-        print(f"{count} records from {args.input} saved to {args.found}")
+if args.found:
+    records = (r for r in SeqIO.parse(args.input, "fasta") if r.id in filter)
+    count = SeqIO.write(records, args.found, "fasta")
+    print(f"{count} of {len(filter)} records from {args.search} found in {args.input}")
+    print(f"{count} records from {args.input} saved to {args.found}")
 
-    if args.notfound:
-        records = (r for r in SeqIO.parse(args.input, "fasta") if r.id not in wanted)
-        count = SeqIO.write(records, args.notfound, "fasta")
-        print(f"{count} records from {args.input} not present in {args.search}")
-        print(f"{count} records from {args.input} saved to {args.notfound}")
+if args.notfound:
+    records = (r for r in SeqIO.parse(args.input, "fasta") if r.id not in filter)
+    count = SeqIO.write(records, args.notfound, "fasta")
+    print(f"{count} records from {args.input} not present in {args.search}")
+    print(f"{count} records from {args.input} saved to {args.notfound}")
 
 print('\n')
