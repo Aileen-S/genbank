f63f0060829e92795e878b447c08d59b63a93187
Aileen-S <a.scott21@imperial.ac.uk>
2023-12-15 19:36:06 +0000

    update

--- lat_lon.R
+++ lat_lon.R
@@ -3,8 +3,9 @@ library(bold)
 library(getopt)
 
 spec <- matrix(c(
-  'taxon',    't', 2, 'character', 'BOLD search term',
-  'output',   'o', 1, 'character', 'Output metadata csv'
+  'taxon',  't', 2, 'character', 'BOLD search term',
+  'input',  'i', 2, 'character', 'BOLD csv file (instead of --taxon)'
+  'output', 'o', 1, 'character', 'Output metadata csv'
 ), byrow = T, ncol = 5)
 
 opt <- getopt(spec)
@@ -12,7 +13,12 @@ opt <- getopt(spec)
 #getwd()
 #setwd('~/OneDrive/Diversity/')
 
-out <- bold_seqspec(taxon=opt$taxon)
+if ( !is.null(opt$taxon) ) {
+  out <- bold_seqspec(taxon=opt$taxon)
+} else {
+  out <- read.csv(opt$input)
+} 
+
 
 print(paste(nrow(out), 'records found for', opt$taxon))
 
@@ -20,19 +26,15 @@ print(paste(nrow(out), 'records found for', opt$taxon))
 meta <- out %>% select(processid, bin_uri, markercode, family_taxID, family_name, 
                        subfamily_taxID, subfamily_name, genus_taxID, genus_name, 
                        species_taxID, species_name, lat, lon)
-str(out)
+#str(out)
 
 # See how many NA in each column
-sapply(meta, function(x) sum(is.na(x)))
+#sapply(meta, function(x) sum(is.na(x)))
 
 # Split dataframe into list of dataframes, one for each 10 degrees of latitude
 lats <- split(meta, cut(meta$lat, seq(-90, 90, 10), include.lowest=TRUE))
 
-length(lats)
-nrow(lats[[5]])
-
 l = seq(-90, 90, 10)
-l[1]
 
 # New empty dataframe
 df <- data.frame(Lat = character(), Rows = numeric(), BINs = numeric(), Species = numeric())
@@ -53,6 +55,8 @@ df$Barcode_BIN_Ratio <- df$Rows/df$BINs
 
 # Barcode: Species ratio
 df$Barcode_Species_Ratio <- df$Rows/df$Species
+
+df
 
 write.csv(df, opt$output, row.names = FALSE)
 
