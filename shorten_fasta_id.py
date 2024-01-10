d42e2c641d04a3dcaa97e2e0515478ff3a13aaa9
Aileen-S <a.scott21@imperial.ac.uk>
2024-01-10 20:58:48 +0000

    update

--- shorten_fasta_id.py
+++ shorten_fasta_id.py
@@ -4,35 +4,24 @@ import csv
 
 parser = argparse.ArgumentParser(description="Rename sequences in fasta file from CSV.")
 parser.add_argument("-i", "--input", type=str, help="Input fasta")
-parser.add_argument("-n", "--names", type=str, help="List of new names (with old name as part of name")
 parser.add_argument("-o", "--output", type=str, help="Output file")
+parser.add_argument("-n", "--number", type=int, help="Strip fasta ID to this many components")
+
 args = parser.parse_args()
 
-new = []
-with open(args.names) as file:
-    lines = file.readlines()
-    for line in lines:
-        line = line.strip()
-        new.append(line)
 
 records = SeqIO.parse(args.input, "fasta")
 output = open(args.output, 'w')
 for rec in records:
     if ';frame=' in rec.id:
         recid, frame = rec.id.split(';')
-        for n in new:
-            if recid in n:
-                rec.id = f'{n};{frame}'
-            elif recid == n:
-                rec.id = f'{n};{frame}'
+        name = recid.split('_')
+        rec.id = f'{"_".join(name[0:args.number])};{frame}'
         output.write(f'>{rec.id}\n{rec.seq}\n')
 
     else:
-        for n in new:
-            if rec.id in n:
-                rec.id = n
-            elif rec.id == n:
-                rec.id = n
+        name = rec.id.split('_')
+        rec.id = f'{"_".join(name[0:3])}'
         output.write(f'>{rec.id}\n{rec.seq}\n')
 
 print(f'Saved renamed fasta to {args.output}')
